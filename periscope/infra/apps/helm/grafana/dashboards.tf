026557185228e54d9bf768fd76360379772176a5
Sadlil Rhythom <sadlil.rhythm@gmail.com>
2023-03-21 17:25:53 +0000

    [periscope] Add custom dashboard support and Loki dashboard support for application

---
+++ periscope/infra/apps/helm/grafana/dashboards.tf
@@ -0,0 +1,18 @@
+variable "namespace" {
+  default = "monitoring"
+}
+
+resource "kubernetes_config_map" "example" {
+  metadata {
+    name = "grafana-custom-dashboards"
+    namespace = var.namespace
+    labels = {
+      "grafana_dashboard" = "1"
+    }
+  }
+
+  data = {
+    "loki_dashboard.json" = "${file("files/grafana/loki_dashboard.json")}"
+    "periscope_dashboard.json" = "${file("files/grafana/periscope_dashboard.json")}"
+  }
+}
--- periscope/infra/apps/helm/helm.tf
+++ periscope/infra/apps/helm/helm.tf
@@ -1,11 +1,16 @@
+module "loki" {
+  source = "./loki"
+}
+
 module "pometheus" {
   source = "./prometheus"
+}
+
+module "grafana" {
+  source = "./grafana"
 }
 
 module "servicemonitor" {
   source = "./servicemonitors"
 }
 
-module "loki" {
-  source = "./loki"
-}
